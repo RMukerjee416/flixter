42340c1362e8ba49cfa7083839750f3f01ffb4a7
Rajkumar <raj_muk@hotmail.com>
2019-06-06 06:10:29 +0000

    Made enrollment mandatory for viewing lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson
 
 def show
 end
 
 private
+
+def require_authorized_for_current_lesson
+	if current_user.enrolled_in?(current_lesson.section.course) == false && current_lesson.section.course.user != current_user
+		flash[:alert] = "You are not enrolled in this course. Please enroll first to view any corresponding lessons. Thank you."
+		redirect_to course_path(current_lesson.section.course)
+	end
+end
 
 helper_method :current_lesson
 def current_lesson
